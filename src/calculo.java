69ce3848f6c001a07499eb4f1826c2af23834d5a
harol chumbe <harbarnard@hotmail.com>
2011-05-29 19:05:53 -0500

    commit el chancha paso base de datos  con lidia!!!
    Change-Id: I6631ac67e7cf9d923c718b91e92df62cc21f1a9c

---
+++ src/calculo.java
@@ -0,0 +1,16 @@
+
+public class calculo {
+
+	public calculo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int suma(int x, int y){
+	return x+y;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
